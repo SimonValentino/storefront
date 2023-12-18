f3e4eff7028245b0679a7b6197587f219eaa7868
Simon Valentino <simontvalentino@gmail.com>
2023-12-17 19:41:59 -0500

    .

--- store/views.py
+++ store/views.py
@@ -1,12 +1,28 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .models import Product
+from .serializers import ProductSerializer
 
 
 @api_view()
 def product_list(request):
-    return Response("ok")
+    qset = Product.objects.select_related('collection').all()
+    serializer = ProductSerializer(
+        qset, many=True, context={'request': request})
+
+    return Response(serializer.data)
 
 
 @api_view()
 def product_detail(request, id):
-    return Response(id)
+    product = get_object_or_404(Product, pk=id)
+    serializer = ProductSerializer(product)
+
+    return Response(serializer.data)
+
+
+@api_view
+def collection_detail(request, id):
+    return Response("ok")
